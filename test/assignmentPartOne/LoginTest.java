b14abb1ed27bb5f71107ee6b1597e10e804a21f3
RC_Student_lab <RC_Student_lab@labG9AEB3.fsduzoceieoebbbscditgazhfa.fx.internal.cloudapp.net>
2025-09-17 18:00:37 +0000

    PART 1 FIRST COMMIT

---
+++ test/assignmentPartOne/LoginTest.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package assignmentPartOne;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    /**
+     * Test of checkUserName method, of class Login.
+     */
+    @Test
+    public void testCheckUserName() {
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+    /**
+     * Test of checkCellPhoneNumber method, of class Login.
+     */
+    @Test
+    public void testCheckCellPhoneNumber() {
+    }
+
+    /**
+     * Test of registerUser method, of class Login.
+     */
+    @Test
+    public void testRegisterUser() {
+    }
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+    }
+
+    /**
+     * Test of returnloginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnloginStatus() {
+    }
+
+    /**
+     * Test of username method, of class Login.
+     */
+    @Test
+    public void testUsername() {
+    }
+
+    /**
+     * Test of password method, of class Login.
+     */
+    @Test
+    public void testPassword() {
+    }
+
+    /**
+     * Test of cellPhone method, of class Login.
+     */
+    @Test
+    public void testCellPhone() {
+    }
+
+    /**
+     * Test of firstName method, of class Login.
+     */
+    @Test
+    public void testFirstName() {
+    }
+
+    /**
+     * Test of lastName method, of class Login.
+     */
+    @Test
+    public void testLastName() {
+    }
+    
+}
